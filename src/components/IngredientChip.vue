5eac40fdd32a75c6fcacab29db6089b6c1dc6d5f
Christine Wu <cwu27@mit.edu>
2025-11-23 21:13:10 -0500

    added ingredient chip component

---
+++ src/components/IngredientChip.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ingredient-chip">
+    <span class="chip-label">{{ label }}</span>
+    <button
+      class="chip-remove"
+      @click="handleRemove"
+      aria-label="Remove ingredient"
+    >
+      ×
+    </button>
+  </div>
+</template>
+
+<script setup>
+import "./IngredientChip.css";
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["remove"]);
+
+function handleRemove() {
+  emit("remove");
+}
+</script>
