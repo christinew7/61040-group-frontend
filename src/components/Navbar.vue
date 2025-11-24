4f31183f65be50d68fc0f39c3b5a3bfe8c72f92a
Christine Wu <cwu27@mit.edu>
2025-11-24 18:22:51 -0500

    added navbar and updated sidebar styling

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="navbar">
+    <h1>{{ title }}</h1>
+    <button @click="handleBack" class="profile-button" title="Go to Profile">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+        <circle cx="12" cy="7" r="4"></circle>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import "./Navbar.css";
+import { useRouter } from "vue-router";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const router = useRouter();
+
+function handleBack() {
+  router.push("/profile");
+}
+</script>
