dc145f967c4009b651722afc47cc1f55ae1ecc06
Eyob Davidoff <eyobdavidoff@college.harvard.edu>
2025-11-29 17:03:31 -0500

    Update styles to SCSS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-// Import theme system
-import "./styles/theme.css";
+// Import global styles (includes theme)
+import "./styles/global.scss";
 
 // Router is optional but App.vue uses <router-view />; create a minimal router
 import router from "./router";
